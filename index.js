7e131cde94865e34f2466435ea08415aa09480fc
Anitha-najani <anithanajani26@gmail.com>
2022-11-25 16:51:54 +0530

    updated postmethod

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.get("/",(req,res)=>{
 
 app.post("/newData",(req,res)=>{
 	//const name=req.body.name,age=req.body.age
-	const{name,password}=req.body
-	console.log(name,password)
+	const{name,password,group,gender}=req.body
+	console.log(name,password,group,gender)
 	res.send("Added Data")
 })
 
